64a77f35505c874282b5c816ae4f91244764f669
Alexey Kopytov <akopytov@gmail.com>
2019-08-16 19:06:44 +0300

    Update copyright.

--- tune.lua
+++ tune.lua
@@ -1,4 +1,4 @@
--- Copyright (C) 2018 Alexey Kopytov <akopytov@gmail.com>
+-- Copyright (C) 2018-2019 Alexey Kopytov <akopytov@gmail.com>
 --
 -- This code is licensed under the MIT license, see LICENSE.
 
